c31400fb8ce913b10457fed40eb44109c96469f2
Arthur <arthur.langenbronn@gmail.com>
2021-04-13 23:03:25 +0200

    spring initializr projet vente en ligne

---
+++ src/main/java/com/venteEnLigne/venteEnLigne/VenteEnLigneApplication.java
@@ -0,0 +1,13 @@
+package com.venteEnLigne.venteEnLigne;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VenteEnLigneApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VenteEnLigneApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/venteEnLigne/venteEnLigne/VenteEnLigneApplicationTests.java
@@ -0,0 +1,13 @@
+package com.venteEnLigne.venteEnLigne;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VenteEnLigneApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

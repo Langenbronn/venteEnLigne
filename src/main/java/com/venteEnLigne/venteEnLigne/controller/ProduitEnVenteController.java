58b4bc4a8c14254561b591c7234f506ca3edb5eb
Arthur <arthur.langenbronn@gmail.com>
2021-05-25 21:19:17 +0200

    ProduitEnVente : findAll

--- src/main/java/com/venteEnLigne/venteEnLigne/controller/ProduitEnVenteController.java
+++ src/main/java/com/venteEnLigne/venteEnLigne/controller/ProduitEnVenteController.java
@@ -7,13 +7,12 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 import java.util.Optional;
 
 @RestController
-@RequestMapping("/api")
+@RequestMapping("/api/produitEnVente")
 public class ProduitEnVenteController {
     @Autowired
     ProduitEnVenteRepository produitEnVenteRepository;
@@ -36,9 +35,42 @@ public class ProduitEnVenteController {
         return "Customer is created";
     }
 
+    @PutMapping("/update/{id}")
+    public ResponseEntity<ProduitEnVente> update(@PathVariable("id") long id, @RequestBody ProduitEnVente produitEnVente) {
+        Optional<ProduitEnVente> produitEnVenteData = produitEnVenteRepository.findById(id);
+
+        if (produitEnVenteData.isPresent()) {
+            ProduitEnVente _produitEnVente = produitEnVenteData.get();
+            _produitEnVente.setNom(produitEnVente.getNom());
+            _produitEnVente.setPrix(produitEnVente.getPrix());
+            _produitEnVente.setDescription(produitEnVente.getDescription());
+            _produitEnVente.setNombreDisponible(produitEnVente.getNombreDisponible());
+            return new ResponseEntity<>(produitEnVenteRepository.save(_produitEnVente), HttpStatus.OK);
+        } else {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+    }
+
+    @DeleteMapping("/delete/{id}")
+    public ResponseEntity<HttpStatus> delete(@PathVariable("id") long id) {
+        try {
+            produitEnVenteRepository.deleteById(id);
+            return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+        } catch (Exception e) {
+            return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
+
     @GetMapping("/read/{id}")
     public ResponseEntity<ProduitEnVente> getProduitById(@PathVariable("id") long id) {
         Optional<ProduitEnVente> produitEnVentelData = produitEnVenteRepository.findById(id);
         return produitEnVentelData.map(produitEnVente -> new ResponseEntity<>(produitEnVente, HttpStatus.OK)).orElseGet(() -> new ResponseEntity<>(HttpStatus.NOT_FOUND));
     }
+
+    @GetMapping("/findAll")
+    public ResponseEntity<List<ProduitEnVente>> finddAll() {
+        List<ProduitEnVente> produitEnVentelData = produitEnVenteRepository.findAll();
+        return new ResponseEntity<>(produitEnVentelData, HttpStatus.OK);
+    }
+
 }
--- src/main/java/com/venteEnLigne/venteEnLigne/repository/ProduitEnVenteRepository.java
+++ src/main/java/com/venteEnLigne/venteEnLigne/repository/ProduitEnVenteRepository.java
@@ -3,6 +3,9 @@ package com.venteEnLigne.venteEnLigne.repository;
 import com.venteEnLigne.venteEnLigne.model.ProduitEnVente;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 
 public interface ProduitEnVenteRepository  extends JpaRepository<ProduitEnVente, Long> {
+//    List<ProduitEnVente> findByPublished(boolean published);
 }

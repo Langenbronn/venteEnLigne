e956d2cbaed2c52ecab1331584c460b721ec1c20
Arthur <arthur.langenbronn@gmail.com>
2021-06-21 23:02:07 +0200

    Change for english

--- src/main/java/com/venteEnLigne/venteEnLigne/OnlineSalesApplication.java
+++ src/main/java/com/venteEnLigne/venteEnLigne/OnlineSalesApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class VenteEnLigneApplication {
+public class OnlineSalesApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(VenteEnLigneApplication.class, args);
+		SpringApplication.run(OnlineSalesApplication.class, args);
 	}
 
 }

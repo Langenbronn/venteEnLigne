485b8793d3de1cfe9affab68f5eb5ebf3630505a
ArthurLangenbronn <34140640+Langenbronn@users.noreply.github.com>
2021-11-22 23:17:52 +0100

    Postgresql data produit fix

--- src/main/java/com/venteEnLigne/venteEnLigne/repository/SellerRepository.java
+++ src/main/java/com/venteEnLigne/venteEnLigne/repository/SellerRepository.java
@@ -3,5 +3,8 @@ package com.venteEnLigne.venteEnLigne.repository;
 import com.venteEnLigne.venteEnLigne.model.Seller;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface SellerRepository extends JpaRepository<Seller, Long> {
+    Optional<Seller> findByName(String name);
 }

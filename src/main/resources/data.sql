69bf7ef1139e2d1dc2c6543d63d5570c94050ad4
Arthur <arthur.langenbronngmail.com>
2022-01-16 23:26:56 +0100

    stock init

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -4,3 +4,8 @@ INSERT INTO public.customer(id, firstname, gender, lastname) VALUES (1, 'Arthur'
 INSERT INTO public.seller(id, name)	VALUES (1, 'Philibert');
 
 INSERT INTO public.product(id, categorie, description, name, price) VALUES (1, 'test', 'test', 'GOT', 12.20);
+
+
+INSERT INTO public.stock(
+	id, quantity, product, seller)
+	VALUES (1, 5, 1, 1);

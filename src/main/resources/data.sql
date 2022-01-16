788ff46065a95fc74940858d2144054daa0a915e
Arthur <arthur.langenbronngmail.com>
2022-01-16 23:07:52 +0100

    test

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,1 +1,6 @@
-INSERT INTO public.customer(id, firstname, gender, lastname) VALUES (25, 'Arthur', 'Man', 'Cuvit');
+INSERT INTO public.customer(id, firstname, gender, lastname) VALUES (1, 'Arthur', 'Man', 'Cuvit');
+
+
+INSERT INTO public.seller(id, name)	VALUES (1, 'Philibert');
+
+INSERT INTO public.product(id, categorie, description, name, price) VALUES (1, 'test', 'test', 'GOT', 12.20);

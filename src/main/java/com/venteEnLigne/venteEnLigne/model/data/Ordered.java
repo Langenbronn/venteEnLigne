a68c5cc512b693d5835d4cf26dd9bf9380ee8b49
Arthur <arthur.langenbronngmail.com>
2022-01-17 22:04:07 +0100

    wip

--- src/main/java/com/venteEnLigne/venteEnLigne/model/data/Ordered.java
+++ src/main/java/com/venteEnLigne/venteEnLigne/model/data/Ordered.java
@@ -10,6 +10,7 @@ import javax.persistence.*;
 import java.io.Serializable;
 import java.util.List;
 import java.util.Objects;
+import java.util.UUID;
 
 @Getter
 @Setter
@@ -20,7 +21,7 @@ import java.util.Objects;
 public class Ordered implements Serializable {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
-    private Long id;
+    private UUID id;
 
     @JoinColumn(name = "customer")
     @OneToOne(fetch = FetchType.EAGER)

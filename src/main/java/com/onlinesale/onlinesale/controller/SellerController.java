47bd6f588f98ebd4d4367a1875bf041163069c44
Langenbronn <arthur.langenbronn@gmail.com>
2022-03-10 19:21:17 +0100

    begin hateoas

--- src/main/java/com/onlinesale/onlinesale/controller/SellerController.java
+++ src/main/java/com/onlinesale/onlinesale/controller/SellerController.java
@@ -1,11 +1,13 @@
 package com.onlinesale.onlinesale.controller;
 
+import com.onlinesale.onlinesale.exception.NotFoundRequestException;
 import com.onlinesale.onlinesale.model.data.Seller;
 import com.onlinesale.onlinesale.model.dto.SellerDto;
 import com.onlinesale.onlinesale.model.mapper.SellerMapper;
 import com.onlinesale.onlinesale.model.view.SellerView;
 import com.onlinesale.onlinesale.service.SellerService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.hateoas.EntityModel;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -13,6 +15,9 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 import java.util.UUID;
 import java.util.stream.Collectors;
+
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.linkTo;
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.methodOn;
 
 @RestController
 @RequestMapping("/api/sellers")
@@ -41,8 +46,14 @@ public class SellerController {
     }
 
     @GetMapping("/{id}")
-    public ResponseEntity<SellerView> findOne(@PathVariable("id") UUID id) {
-        return sellerService.findOne(id).map(seller -> new ResponseEntity<>(sellerMapper.entityToView(seller), HttpStatus.OK)).orElseGet(() -> new ResponseEntity<>(HttpStatus.NOT_FOUND));
+    public EntityModel<SellerView> findOne(@PathVariable("id") UUID id) {
+        SellerView sellerView = sellerService.findOne(id)
+                .map(seller -> sellerMapper.entityToView(seller))
+                .orElseThrow(() -> new NotFoundRequestException("seller " + id + " does not exist"));
+
+        return EntityModel.of(sellerView,
+                linkTo(methodOn(SellerController.class).findOne(id)).withSelfRel(),
+                linkTo(methodOn(SellerController.class).findAll()).withRel("sellersss"));
     }
 
     @GetMapping

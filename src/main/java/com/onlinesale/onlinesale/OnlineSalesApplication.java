db111f64877d869b72795b0a09ae1ecbaf61d749
Langenbronn <arthur.langenbronn@gmail.com>
2022-03-20 14:54:26 +0100

    config fix view

--- src/main/java/com/onlinesale/onlinesale/OnlineSalesApplication.java
+++ src/main/java/com/onlinesale/onlinesale/OnlineSalesApplication.java
@@ -1,11 +1,14 @@
 package com.onlinesale.onlinesale;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.hateoas.HypermediaAutoConfiguration;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
 @EnableSwagger2
+@EnableAutoConfiguration(exclude = HypermediaAutoConfiguration.class)
 public class OnlineSalesApplication {
 
     public static void main(String[] args) {
